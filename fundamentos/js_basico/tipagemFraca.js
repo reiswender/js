85f31ffbc848b384fd6351bddaed71d4491fce2d
Wender Freitas Reis <reiswender@gmail.com>
2025-11-26 21:22:50 -0300

    Adicionando os arquivos comentarios, organização, tipagemFraca e var_const_let

---
+++ fundamentos/js_basico/tipagemFraca.js
@@ -0,0 +1,14 @@
+let teste = "teste"
+
+console.log(teste)
+console.log(typeof teste)
+
+console.log()
+
+teste = 3.1516
+console.log(teste)
+console.log(typeof teste)
+
+// Evitar usar termos genéricos e siglas para nomes de variáveis
+let valor
+let numero
---
+++ fundamentos/js_basico/var_const_let.js
@@ -0,0 +1,8 @@
+var a = 3
+let b = 4
+
+console.log(a, b)
+
+const C = 5
+
+console.log(a, b, C)
