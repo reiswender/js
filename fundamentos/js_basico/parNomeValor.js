9be49ea1986b4b3e83b513c157b56cf019dd575a
Wender Freitas Reis <reiswender@gmail.com>
2025-11-30 12:08:17 -0300

    Subindo mais nove arquivos

---
+++ fundamentos/js_basico/parNomeValor.js
@@ -0,0 +1,22 @@
+// Par nome/valor
+const saudacao = 'Opa'
+
+function exec() {
+    const saudacao = 'Faaaala!'
+    return saudacao
+}
+
+// Objetos são grupos aninhados de pares nome/valor
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua Muito Legal',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
